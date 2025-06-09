9948d628987eccc537dcbb310058a050f1333fc2
vanGamo <vangamo.beta@gmail.com>
2025-06-09 20:18:57 +0200

    0.1.4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import JsonCrud from './lib/port/jsonPort.js';
+import TsvCrud from './lib/port/tsvPort.js';
 import FileCrud from './lib/port/filePort.js';
 
 export {
   JsonCrud,
+  TsvCrud,
   FileCrud
 };
