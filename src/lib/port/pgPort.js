f5534a0a0fbf907717d03d163640094793c86281
vanGamo <vangamo.beta@gmail.com>
2025-06-14 12:07:33 +0200

    Add relations to pgPort.

--- src/lib/port/pgPort.js
+++ src/lib/port/pgPort.js
@@ -15,6 +15,129 @@ export default class PgCrud {
 
     this.connectionString = settings.connectionString;
     this.domain = settings.domain;
+    this.primaryKey = settings.primaryKey || 'id';
+    this.relations = Array.isArray(settings.relations)
+      ? settings.relations
+      : typeof settings.relations === 'string'
+      ? [settings.relations]
+      : [];
+  }
+
+  setPrimaryKey(primaryKey) {
+    this.primaryKey = primaryKey;
+  }
+
+  addRelation1N(relation) {
+    if (typeof relation !== 'object') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param error`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param error`,
+        relation
+      );
+    }
+    if (typeof relation.localKey !== 'string') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing localKey`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing localKey`,
+        relation
+      );
+    }
+    if (typeof relation.foreignTable !== 'string') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param : Missing foreignTable`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param : Missing foreignTable`,
+        relation
+      );
+    }
+    if (typeof relation.localKeyInForeignTable !== 'string') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param : Missing localKeyInForeignTable`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param : Missing localKeyInForeignTable`,
+        relation
+      );
+    }
+    this.relations.push({
+      type: '1:N',
+      localKey: relation.localKey,
+      foreignTable: relation.foreignTable,
+      localKeyInForeignTable: relation.localKeyInForeignTable,
+      primaryKeyInForeignTable: relation.primaryKeyInForeignTable || 'id',
+    });
+  }
+
+  addRelationNM(relation) {
+    if (typeof relation !== 'object') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param error`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param error`,
+        relation
+      );
+    }
+    if (typeof relation.relTable !== 'string') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing relTable`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing relTable`,
+        relation
+      );
+    }
+    if (typeof relation.localKeyInRelTable !== 'string') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing localKeyInRelTable`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing localKeyInRelTable`,
+        relation
+      );
+    }
+    if (typeof relation.foreignKeyInRelTable !== 'string') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing foreignKeyInRelTable`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing foreignKeyInRelTable`,
+        relation
+      );
+    }
+    if (typeof relation.foreignTable !== 'string') {
+      console.error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing foreignTable`,
+        relation
+      );
+      throw new Error(
+        `pgPort.addRelation for ${this.domain} has a param error: Missing foreignTable`,
+        relation
+      );
+    }
+
+    this.relations.push({
+      type: 'N:M',
+      localKey: relation.localKey || this.primaryKey,
+      relTable: relation.relTable,
+      localKeyInRelTable: relation.localKeyInRelTable,
+      foreignKeyInRelTable: relation.foreignKeyInRelTable,
+      foreignTable: relation.foreignTable,
+      primaryKeyInForeignTable: relation.primaryKeyInForeignTable || 'id',
+    });
   }
 
   async getConn() {
@@ -40,17 +163,12 @@ export default class PgCrud {
       return null;
     }
 
-    const db = await this.getConn();
-    let resultSet;
+    let result;
     if (Number.isInteger(condition) && condition > 0) {
-      resultSet = await db.query(`SELECT * FROM ${this.domain} WHERE id=$1`, [
-        condition,
-      ]);
+      result = await this.read({ [this.primaryKey]: condition });
     } else if (typeof condition === 'string') {
       // #TODO: check unique and string fields
-      resultSet = await db.query(`SELECT * FROM ${this.domain} WHERE name=$1`, [
-        condition,
-      ]);
+      result = await this.read({ name: condition });
     } else if (
       typeof condition !== 'object' ||
       Object.keys(condition).length === 0
@@ -60,41 +178,98 @@ export default class PgCrud {
       // #TODO: check unique fields
       const [field, value] = Object.entries(condition).at(0);
 
-      resultSet = await db.query(
-        `SELECT * FROM ${this.domain} WHERE ${field}=$1`,
-        [value]
-      );
+      result = await this.read({ [field]: value });
     }
-    await db.release();
 
-    return resultSet.rows.length === 1 ? resultSet.rows : null;
+    return result.length === 1 ? result : null;
   }
 
-  async read(condition) {
+  async read(readConditions = {}) {
     const db = await this.getConn();
     let resultSet;
-    if (!condition || typeof condition !== 'object') {
-      resultSet = await db.query(`SELECT * FROM ${this.domain}`);
-    } else {
-      const queryParams = Object.entries(condition).reduce(
-        (queryParams, [field, value], idx) => ({
-          conditions: [...queryParams.conditions, `"${field}" = $${1 + idx}`],
-          values: [...queryParams.values, value],
-        }),
-        { conditions: [], values: [] }
-      );
 
-      resultSet = await db.query(
-        `SELECT * FROM ${this.domain} WHERE ${queryParams.conditions.join(
-          ' AND '
-        )}`,
-        queryParams.values
+    let [fields, conditions, joins, values, groupByFields] = [
+      [],
+      [],
+      [],
+      [],
+      [],
+    ];
+
+    fields = ['*'];
+
+    [fields, conditions, joins, values] = Object.entries(readConditions).reduce(
+      ([fields, conditions, joins, values], [field, value], idx) => [
+        /*fields:*/ fields,
+        /*conditions:*/ [...conditions, `"${field}" = $${1 + idx}`],
+        /*joins:*/ joins,
+        /*values:*/ [...values, value],
+      ],
+      [fields, conditions, joins, values]
+    );
+
+    if (this.relations.length > 0) {
+      fields = fields.map((field) => `${this.domain}.${field}`);
+
+      [fields, conditions, joins, values] = this.relations.reduce(
+        ([fields, conditions, joins, values], relation, idx) =>
+          relation.type === 'N:M'
+            ? [
+                /*fields:*/ [
+                  ...fields,
+                  `array_agg(${relation.relTable}.${relation.foreignKeyInRelTable}) AS ${relation.foreignTable}`,
+                ],
+                /*conditions:*/ conditions,
+                /*joins:*/ [
+                  ...joins,
+                  `LEFT JOIN ${relation.relTable} ON (${this.domain}.${relation.localKey} = ${relation.relTable}.${relation.localKeyInRelTable})`,
+                ],
+                /*values:*/ values,
+              ]
+            : [
+                /*fields:*/ [
+                  ...fields,
+                  `array_agg(${relation.foreignTable}.${relation.primaryKeyInForeignTable}) AS ${relation.foreignTable}`,
+                ],
+                /*conditions:*/ conditions,
+                /*joins:*/ [
+                  ...joins,
+                  `LEFT JOIN ${relation.foreignTable} ON (${this.domain}.${relation.localKey} = ${relation.foreignTable}.${relation.localKeyInForeignTable})`,
+                ],
+                /*values:*/ values,
+              ],
+        [fields, conditions, joins, values]
       );
+      groupByFields = [`${this.domain}.${this.primaryKey}`];
     }
+
+    const queryStmt = `
+        SELECT ${fields.join(', ')}
+          FROM ${this.domain}
+            ${joins.length === 0 ? '' : joins.join('\n')}
+          ${conditions.length === 0 ? '' : ' WHERE ' + conditions.join(' AND ')}
+          ${
+            groupByFields.length === 0
+              ? ''
+              : 'GROUP BY ' + groupByFields.join(', ')
+          }`;
+
+    resultSet = await db.query(queryStmt, values);
 
     await db.release();
 
-    return resultSet.rows;
+    return resultSet.rows.map((it) => {
+      this.relations.forEach((relation) => {
+        if (
+          Array.isArray(it[relation.foreignTable]) &&
+          it[relation.foreignTable].length === 1 &&
+          it[relation.foreignTable].at(0) === null
+        ) {
+          it[relation.foreignTable] = [];
+        }
+      });
+      return it;
+    });
   }
 
   async create(item) {
